e87f91f749e7c5447a3e120475e411e722c87c2e
Aisea Tawake <tawaab1@student.op.ac.nz>
2020-03-27 15:05:18 +1300

    commiting what I have done so far

---
+++ Web app/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, redirect, url_for
+from mongoengine import *
+import os
+import csv
+connect ('DB_USERS')
+
+class User(Document):
+    email = StringField(required=True)
+    first_name = StringField(max_length = 50)
+    last_name = StringField(max_length = 50)
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+namePage= "Aisea Busa Tawake"
+
+@app.route('/')
+def index():
+    aisea = User(email = 'tawaab1@student.op.ac.nz', first_name = 'Aisea', last_name = 'Tawake')
+    aisea.save()
+    for file in os.listdir(app.config['FILES_FOLDER']):
+        filename = os.fsdecode(file)
+        path = os.path.join(app.config['FILES_FOLDER'], filename)
+        f = open(path)
+        r = csv.reader(f)
+        d = list(r)
+        for data in d:
+                print(data)
+    return render_template('index.html', name=namePage)
+
+@app.route('/inspiration')
+def inspiration():
+    return render_template('inspiration.html',name=namePage)
+
+
+
+if __name__ =="__main__":
+     app.run(debug=True, port=8080)
+
+
+@app.route('/getUsers', methods=['GET'])
+def getUsers():
+    users = User.objects
+    return users.to_json()
+
+
---
+++ Web app/config.py
@@ -0,0 +1,5 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+FILES_FOLDER = os.path.join(basedir,'files')
