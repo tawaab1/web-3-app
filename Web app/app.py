fc3cd3a033ccfc7b58bc8d69d52bc4846b0a2f45
Aisea Tawake <tawaab1@student.op.ac.nz>
2020-03-28 10:34:01 +1300

    adding app.run on the web server, was using the local instead

--- Web app/app.py
+++ Web app/app.py
@@ -36,7 +36,8 @@ def inspiration():
 
 
 if __name__ =="__main__":
-     app.run(debug=True, port=8080)
+	 app.run(debug=True, port=80, host='0.0.0.0')
+     #app.run(debug=True, port=8080)
 
 
 @app.route('/getUsers', methods=['GET'])
