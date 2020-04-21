59e5750b2dfb4fe6b90a5a95e609e62857f17090
Aisea Tawake <tawaab1@student.op.ac.nz>
2020-04-21 15:10:10 +1200

    direct copy of the code see if it fixes indent problem

--- Web app/app.py
+++ Web app/app.py
@@ -30,18 +30,26 @@ def index():
         r = csv.reader(f)
         d = list(r)
         for data in d:
-                country = Country()
-                dict = {}
-                for key in data:
-                  if key == "country":
-                    else:
-                      f = filename.replace(".csv","")
-                  if f in dict:
-                      dict[f][key] = data[key]
-                    else:
-                        dict[f] = {key:data[key]}
+        country = Country() # a blank placeholder country
+        dict = {} # a blank placeholder data dict
+        for key in data: # iterate through the header keys
+            if key == "country":
+                # check if this country already exists in the db
+                
+                # if the country does not exist, we can use the new blank country we created above, and set the name
 
-                        country.save()
+                # if the country already exists, replace the blank country with the existing country from the db, and replace the blank dict with the current country's 
+                # data                
+            else:
+                f = filename.replace(".csv","") # we want to trim off the ".csv" as we can't save anything with a "." as a mongodb field name
+                if f in dict: # check if this filename is already a field in the dict
+                    dict[f][key] = data[key] # if it is, just add a new subfield which is key : data[key] (value)
+                else:
+                    dict[f] = {key:data[key]} # if it is not, create a new object and assign it to the dict
+
+            # add the data dict to the country
+
+        # save the country
     return render_template('index.html', name=namePage)
 
 @app.route('/inspiration')
